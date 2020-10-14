3d5c86b3562fe0ffa2f99410497553d3c0358a04
Dmitry Kmita <dmitry@travonto.com>
2020-10-14 11:11:42 +0100

    Initial commit with template, server confguration and readme

---
+++ README.md
@@ -0,0 +1,27 @@
+#Innowait Solutions Tutorial 01
+##A-Z Coming Soon Website
+
+Welcome to Innowait Solutions support docs for our tutorials.
+
+Let's create a website!
+
+###Template
+* Template for the Coming Soon Website can be found in `template/` folder!
+* You can amend images in `template/images` folder including backgrounds and logo!
+* Placeholders in index.html which you can just "Find & Replace":
+  * `%COMPANY_NAME%` - Your Company Name
+  * `%YEAR%` - Current Year
+  * `%FACEBOOK_URL%` - URL to your Facebook Profile
+  * `%TWITTER_URL%` - URL to your Twitter Profile
+  * `%YOUTUBE_URL%` - URL to your YouTube Profile
+  * `%INSTAGRAM_URL%` - URL to your Instagram Profile
+* Current main color is #700000 in `main.css`, again you can just "Find & Replace" with the color which matches your brand!
+
+###Server Configuration
+* Install nginx on your server using command (for Ubuntu):
+```sudo apt-get install nginx```
+
+* Website configuration file has to sit under `/etc/nginx/sites-enabled/%YOUR-NAME%`
+* To reload configuration for nginx use command
+```sudo service nginx reload```
+* NGINX Configuration to listen for requests on port 80 and direct to your html website can be found in a `server/website` file.
---
+++ template/js/main.js
@@ -0,0 +1,78 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+	/* --------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+  	})
+
+
+	/*-----------------------------------------------------*/
+	/* tabs
+  	-------------------------------------------------------*/	
+	$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("ul.tabs li").click(function () {
+	   $("ul.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});
+
+
+	/*----------------------------------------------------*/
+  	/* Smooth Scrolling
+  	------------------------------------------------------*/
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+
+	/*----------------------------------------------------*/
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2021/01/01';
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+
+   		$(this).html(event.strftime('<div class=\"half\">' +
+   											 '<span>%D <sup>days</sup></span>' + 
+   										 	 '<span>%H <sup>hours</sup></span>' + 
+   										 	 '</div>' +
+   										 	 '<div class=\"half\">' +
+   										 	 '<span>%M <sup>mins</sup></span>' +
+   										 	 '<span>%S <sup>secs</sup></span>' +
+   										 	 '</div>'));
+
+   });     
+ 
+
+})(jQuery);
